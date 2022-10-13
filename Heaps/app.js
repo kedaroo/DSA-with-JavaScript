2e1204e7c2c1a80c24ce6e1ce054a3c10ee85dcc
Kedar Basutkar <krb1202@gmail.com>
2022-10-13 18:54:01 +0530

    add: extract method

--- Heaps/app.js
+++ Heaps/app.js
@@ -3,6 +3,12 @@ class MaxBinaryHeap {
     this.values = [];
   }
 
+  _swap(idx1, idx2) {
+    const temp = this.values[idx1];
+    this.values[idx1] = this.values[idx2];
+    this.values[idx2] = temp;
+  }
+
   insert(element) {
     this.values.push(element);
 
@@ -21,13 +27,47 @@ class MaxBinaryHeap {
 
     return this.values;
   }
+
+  extract() {
+    if (this.values.length > 0) {
+      this._swap(0, this.values.length - 1);
+      const root = this.values.pop();
+
+      let index = 0;
+      let leftChildIdx = 2 * index + 1;
+      let rightChildIdx = 2 * index + 2;
+      let leftChild = this.values[leftChildIdx] ?? -Infinity;
+      let rightChild = this.values[rightChildIdx] ?? -Infinity;
+
+      let maxChildIdx = leftChild > rightChild ? leftChildIdx : rightChildIdx;
+
+      while (this.values[index] < this.values[maxChildIdx]) {
+        this._swap(index, maxChildIdx);
+
+        index = maxChildIdx;
+        leftChildIdx = 2 * index + 1;
+        rightChildIdx = 2 * index + 2;
+        leftChild = this.values[leftChildIdx] ?? -Infinity;
+        rightChild = this.values[rightChildIdx] ?? -Infinity;
+        maxChildIdx = leftChild > rightChild ? leftChildIdx : rightChildIdx;
+      }
+
+      return root;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
-console.log(heap.insert(41));
-console.log(heap.insert(39));
-console.log(heap.insert(33));
-console.log(heap.insert(18));
-console.log(heap.insert(27));
 console.log(heap.insert(12));
+console.log(heap.insert(41));
+console.log(heap.insert(33));
 console.log(heap.insert(55));
+console.log(heap.insert(18));
+
+console.log(heap.extract());
+console.log(heap.extract());
+console.log(heap.extract());
+console.log(heap.extract());
+console.log(heap.extract());
+console.log(heap.extract());
+console.log(heap);
