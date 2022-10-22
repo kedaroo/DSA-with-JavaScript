d8d4118e615a6b2cece76d1de5ee6c59d3633856
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2022-10-22 13:58:45 +0530

    add: naive priority queue

--- Dijkstra's Algorithm/app.js
+++ Dijkstra's Algorithm/app.js
@@ -1,3 +1,22 @@
+class PriorityQueue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(value, priority) {
+    this.queue.push({ value, priority });
+    this.sort();
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  sort() {
+    this.queue.sort((a, b) => a.priority - b.priority);
+  }
+}
+
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
@@ -11,5 +30,4 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
-
 }
